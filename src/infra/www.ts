6f6fbfaf3d4b9396c28ec845c501b9e9c62568c0
kaduhod <carlosjr.ribas@gmail.com.br>
2024-08-28 20:19:53 -0300

    :hammer: Ajustado imports no arquivo de handlers da api

--- src/infra/www.ts
+++ src/infra/www.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import * as UseCases from '../domain/usecases'
-import { isNumeric } from '../main'
+import * as Global from "../global"
 
 export function startServer(db:Array<UseCases.Account>){
     const server = express()
@@ -10,7 +10,7 @@ export function startServer(db:Array<UseCases.Account>){
     })
     server.get('/balance', (req, res) => {
         const account_id = req.query.account_id as string
-        if(!account_id || !isNumeric(account_id)) {
+        if(!account_id || !Global.isNumeric(account_id)) {
             return res.status(400).send();
         }
         const {
@@ -19,7 +19,7 @@ export function startServer(db:Array<UseCases.Account>){
         } = UseCases.GetBalanceFromAccount(account_id);
 
         if(errorCode) {
-            if(errorCode == UseCases.NON_EXISTING_ACCOUNT_ERR) {
+            if(errorCode == Global.NON_EXISTING_ACCOUNT_ERR) {
                 return res.status(404).send("0");
             } else {
                 return res.status(500).send();
