be340934d1b362e9598cf81f7077c20e131560f5
kaduhod <carlosjr.ribas@gmail.com.br>
2024-08-28 20:42:58 -0300

    :fire: Retirnado funcao que nao será mais utilizada

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -1,10 +1,4 @@
 import { Account } from "../domain/usecases";
-import { DefaultFunctionReturn } from "../main";
-export interface DbHandler {
-    GetAccount(account_id:string):DefaultFunctionReturn<Account|null>
-    CreateAccount(account_id:string, amount:number):DefaultFunctionReturn<Account|null>
-    UpdateAccount(account_id:string, values:Account):DefaultFunctionReturn<Account|null>
-}
 const db:Array<Account> = [];
 export const GetDb = ():Array<Account> => {
     return db;
