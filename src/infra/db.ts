b2207a4d9f630c7987f10e39d65e8fbf7ed80891
kaduhod <carlosjr.ribas@gmail.com.br>
2024-08-28 23:11:45 -0300

    :bug: Corrigifo import

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -1,4 +1,4 @@
-import { Account } from "../domain/usecases";
+import { Account } from "../domain/entities";
 const db:Array<Account> = [];
 export const GetDb = ():Array<Account> => {
     return db;
