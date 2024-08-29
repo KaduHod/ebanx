e38af0357c4f5474a878741d81ae3ef0f86728b6
kaduhod <carlosjr.ribas@gmail.com.br>
2024-08-28 23:13:41 -0300

    :construction: Adicionado handlers http e inicializacao de servidor de api

--- src/infra/www.ts
+++ src/infra/www.ts
@@ -1,47 +1,61 @@
 import express from 'express'
 import * as UseCases from '../domain/usecases'
 import * as Global from "../global"
+import { Account } from '../domain/entities'
 
-export function startServer(db:Array<UseCases.Account>){
+export function startServer(db:Account[]){
     const server = express()
+    server.use(express.json())
     server.post('/reset', (_, res) => {
         db = UseCases.Reset(db);
         res.status(200).send();
     })
     server.get('/balance', (req, res) => {
-        const account_id = req.query.account_id as string
-        if(!account_id || !Global.isNumeric(account_id)) {
-            return res.status(400).send();
+        const account_id = req.query.account_id
+        if(!account_id || !Global.isNumeric(account_id as string) || typeof account_id != 'string') {
+            return res.sendStatus(400);
         }
-        const {
-            errorCode,
-            data
-        } = UseCases.GetBalanceFromAccount(account_id);
-
+        const { errorCode, data } = UseCases.GetBalanceFromAccount(db, account_id);
         if(errorCode) {
-            if(errorCode == Global.NON_EXISTING_ACCOUNT_ERR) {
-                return res.status(404).send("0");
-            } else {
-                return res.status(500).send();
-            }
+            return res.status(
+                Global.NON_EXISTING_ACCOUNT_ERR == errorCode ? 404 : 500
+            ).send("0");
         }
 
-        return res.status(200).send(data)
+        return res.status(200).send(String(data))
     })
     server.post('/event', (req, res) => {
         const event = req.body
-        if(!event.type || !event.amount) {
-            return res.status(400).send(0)
+        if(!event?.type || !event?.amount) {
+            return res.sendStatus(400);
         }
-        const {errorCode, data} = UseCases.handleEvent(event)
         switch (event.type) {
             case 'deposit':
-                break;
+                const depositResult = UseCases.Deposit(db, event.destination, event.amount)
+                if(depositResult.errorCode) {
+                    return res.status(
+                        depositResult.errorCode == Global.NON_EXISTING_ACCOUNT_ERR ? 404 : 500
+                    ).send("0");
+                }
+                return res.status(200).send(depositResult.data)
             case 'transfer':
-                break;
+                var transferResult = UseCases.Transfer(db, event.origin, event.destination, event.amount)
+                if(transferResult.errorCode) {
+                    return res.status(
+                        transferResult.errorCode == Global.NON_EXISTING_ACCOUNT_ERR ? 404 : 500
+                    ).send("0");
+                }
+                return res.status(201).send(transferResult.data)
             case 'withdraw':
-                break;
+                const withdrawResult = UseCases.Withdraw(db, event.origin, event.amount);
+                if(withdrawResult.errorCode) {
+                    return res.status(
+                        withdrawResult.errorCode == Global.NON_EXISTING_ACCOUNT_ERR ? 404 : 500
+                    ).send("0")
+                }
+                return res.status(200).json(withdrawResult.data)
             default:
+                res.sendStatus(500);
                 break;
         }
     })
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+import { GetDb } from "./infra/db"
+import { startServer } from "./infra/www"
 
+const main = async () => {
+    console.log("############### Starting home test ###############")
+    startServer(GetDb())
+}
+main();
